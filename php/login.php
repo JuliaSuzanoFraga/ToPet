d8f2bc788c6521490bcf059763fa8057a7e73b9c
Mariana Tassan <51093986+marianatassan@users.noreply.github.com>
2020-09-24 09:29:51 -0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,1 +1,14 @@
+<?php
+	$login = "mariana";
+	$senha = "123";
 
+	if(isset($_POST["enviar"])){
+		if($_POST["login"] == $login && $_POST["senha"] == $senha){
+			echo "Login efetuado com sucesso";
+		}
+		else{
+			echo "Login ou senha inválido";
+		}
+	}
+
+?>
